d6844d09df5f4c9d40c7da1a7d1574bb410b737f
kanishkaag <kanishkaagrawal28@gmail.com>
2026-01-07 02:20:55 +0530

    Improve database configuration

--- payment_logger_service/database.py
+++ payment_logger_service/database.py
@@ -1,3 +1,4 @@
+# Setup SQLAlchemy MySQL connection using environment variables
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
